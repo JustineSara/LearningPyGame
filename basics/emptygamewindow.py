aa31a3f26aa4e3d6e55e4b46a0900417e24d3d85
Justine Sara Pujos <pujos.justine.s@gmail.com>
2023-07-28 17:05:05 +0200

    feat : basicsm create a game window

---
+++ basics/emptygamewindow.py
@@ -0,0 +1,36 @@
+import pygame, sys
+from pygame.locals import *
+
+
+## Define globals ##
+
+# zindoz size in pixels
+WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 300
+
+
+def main():
+# main : main function with the game loop
+
+    pygame.init()
+    displysurf = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+    pygame.display.set_caption('Hello World!')
+    # this is the name of the window, typically the name of the game
+
+    # this is the game loop
+    while True :
+
+        # consider all events
+        for event in pygame.event.get() :
+            if event.type == QUIT :
+                pygame.quit()
+                sys.exit()
+        
+        # update the view of the game
+        pygame.display.update()
+
+
+
+
+if __name__ == '__main__':
+    main()
